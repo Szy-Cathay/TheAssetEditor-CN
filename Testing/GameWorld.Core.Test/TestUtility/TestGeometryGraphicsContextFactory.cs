51278e5dac16102f79234b0b085b80739206af02
热心市民石先生 <1249467256@qq.com>
2026-07-27 14:26:32 +0800

    test: record geometry upload ranges

--- Testing/GameWorld.Core.Test/TestUtility/TestGeometryGraphicsContextFactory.cs
+++ Testing/GameWorld.Core.Test/TestUtility/TestGeometryGraphicsContextFactory.cs
@@ -4,19 +4,33 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace GameWorld.Core.Test.TestUtility
 {
+    public sealed record PartialUpload(int StartIndex, int Count);
+
     public class TestGeometryGraphicsContextFactory : IGeometryGraphicsContextFactory
     {
-        public IGraphicsCardGeometry Create() => new TestGraphicsCardGeometry();
+        private readonly List<TestGraphicsCardGeometry> _createdContexts = new();
+
+        public IReadOnlyList<TestGraphicsCardGeometry> CreatedContexts => _createdContexts;
+
+        public IGraphicsCardGeometry Create()
+        {
+            var context = new TestGraphicsCardGeometry();
+            _createdContexts.Add(context);
+            return context;
+        }
     }
 
     public class TestGraphicsCardGeometry : IGraphicsCardGeometry
     {
+        private readonly List<PartialUpload> _partialUploads = new();
+
         public IndexBuffer IndexBuffer { get; }
         public VertexBuffer VertexBuffer { get; }
         public int IndexBufferRebuildCount { get; private set; }
         public int VertexBufferRebuildCount { get; private set; }
         public int PartialVertexBufferRebuildCount { get; private set; }
         public VertexPositionNormalTextureCustom[] UploadedVertexArray { get; private set; }
+        public IReadOnlyList<PartialUpload> PartialUploads => _partialUploads;
 
         public void RebuildIndexBuffer(ushort[] indexList)
         {
@@ -30,6 +44,7 @@ namespace GameWorld.Core.Test.TestUtility
         public void RebuildVertexBufferPartial(VertexPositionNormalTextureCustom[] vertArray, int startIndex, int count, VertexDeclaration vertexDeclaration, int vertexStride)
         {
             PartialVertexBufferRebuildCount++;
+            _partialUploads.Add(new PartialUpload(startIndex, count));
             UploadedVertexArray ??= vertArray.ToArray();
             Array.Copy(vertArray, startIndex, UploadedVertexArray, startIndex, count);
         }
@@ -39,6 +54,7 @@ namespace GameWorld.Core.Test.TestUtility
             IndexBufferRebuildCount = 0;
             VertexBufferRebuildCount = 0;
             PartialVertexBufferRebuildCount = 0;
+            _partialUploads.Clear();
         }
 
         public IGraphicsCardGeometry Clone() { return this; }
