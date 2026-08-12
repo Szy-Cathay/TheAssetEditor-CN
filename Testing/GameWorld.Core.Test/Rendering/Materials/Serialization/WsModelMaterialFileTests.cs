71ef1eb1197297e3a80df6a6ccc65f8eed8cf34b
热心市民石先生 <1249467256@qq.com>
2026-08-12 19:53:51 +0800

    fix: support wsmodel surface map slots

---
+++ Testing/GameWorld.Core.Test/Rendering/Materials/Serialization/WsModelMaterialFileTests.cs
@@ -0,0 +1,31 @@
+﻿using Shared.GameFormats.RigidModel.Types;
+using Shared.GameFormats.WsModel;
+
+namespace GameWorld.Core.Test.Rendering.Materials.Serialization
+{
+    internal class WsModelMaterialFileTests
+    {
+        [TestCase("s_xml_surface_map")]
+        [TestCase("t_xml_material_map")]
+        public void Constructor_MapsMaterialTextureSlots(string slotName)
+        {
+            const string texturePath = "VariantMeshes/_VariantModels/character/material_map.dds";
+            var materialXml = $"""
+                <material>
+                  <textures>
+                    <texture>
+                      <slot version="2">{slotName}</slot>
+                      <source>{texturePath}</source>
+                    </texture>
+                  </textures>
+                </material>
+                """;
+
+            var material = new WsModelMaterialFile(materialXml);
+
+            Assert.That(
+                material.Textures[TextureType.MaterialMap],
+                Is.EqualTo(texturePath));
+        }
+    }
+}
