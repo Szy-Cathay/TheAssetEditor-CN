f69d01560b2b0b4164932841e0d1956a0a0e9675
热心市民石先生 <1249467256@qq.com>
2026-08-31 03:04:30 +0800

    feat(animation): add time and format capability contracts (#137)

---
+++ Shared/GameFiles/Animation/AnimationFormatCapabilities.cs
@@ -0,0 +1,57 @@
+﻿namespace Shared.GameFormats.Animation
+{
+    public enum AnimationFormatBlockReason
+    {
+        UnsupportedVersion,
+        VersionEightIsReadOnly,
+        MultiplePartsAreReadOnly,
+    }
+
+    public sealed class AnimationFormatCapabilities
+    {
+        private AnimationFormatCapabilities(
+            bool canRead,
+            bool canEdit,
+            bool canSave,
+            IReadOnlyList<AnimationFormatBlockReason> blockingReasons)
+        {
+            CanRead = canRead;
+            CanEdit = canEdit;
+            CanSave = canSave;
+            BlockingReasons = Array.AsReadOnly(blockingReasons.ToArray());
+        }
+
+        public bool CanRead { get; }
+
+        public bool CanEdit { get; }
+
+        public bool CanSave { get; }
+
+        public IReadOnlyList<AnimationFormatBlockReason> BlockingReasons { get; }
+
+        public static AnimationFormatCapabilities Evaluate(uint version, int partCount)
+        {
+            if (version is < 4 or > 8)
+            {
+                return new AnimationFormatCapabilities(
+                    canRead: false,
+                    canEdit: false,
+                    canSave: false,
+                    [AnimationFormatBlockReason.UnsupportedVersion]);
+            }
+
+            var blockingReasons = new List<AnimationFormatBlockReason>();
+            if (version == 8)
+                blockingReasons.Add(AnimationFormatBlockReason.VersionEightIsReadOnly);
+            if (partCount != 1)
+                blockingReasons.Add(AnimationFormatBlockReason.MultiplePartsAreReadOnly);
+
+            var canEditAndSave = blockingReasons.Count == 0;
+            return new AnimationFormatCapabilities(
+                canRead: true,
+                canEdit: canEditAndSave,
+                canSave: canEditAndSave,
+                blockingReasons);
+        }
+    }
+}
